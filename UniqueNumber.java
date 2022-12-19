0cdf0025d6cd523248f165dde9582fd6d796c96c
Rajnish Kumar Singh <hi@hexaorzo.net>
2022-12-19 21:00:46 +0530

    Create UniqueNumber.java

--- UniqueNumber.java
+++ UniqueNumber.java
@@ -11,38 +11,43 @@ public class UniqueNumber
         int n = sc.nextInt();
         if(m<n)
         {
-            System.out.println("The Unique-Digit integers are:");
-            for (int i = m; i <= n; i++) 
+            if(m<30000&&n<30000)
             {
-                boolean unique = true;
-                int num = i;
-                while (num > 0) 
+                System.out.println("The Unique-Digit integers are:");
+                for (int i = m; i <= n; i++) 
                 {
-                    int digit = num % 10;
-                    int temp = num / 10;
-                    while (temp > 0) 
+                    boolean unique = true;
+                    int num = i;
+                    while (num > 0) 
                     {
-                        if (digit == temp % 10) 
+                        int digit = num % 10;
+                        int temp = num / 10;
+                        while (temp > 0) 
                         {
-                            unique = false;
-                            break;
+                            if (digit == temp % 10) 
+                            {
+                                unique = false;
+                                break;
+                            }
+                            temp /= 10;
                         }
-                        temp /= 10;
+                        if (!unique)
+                            break;
+                        num /= 10;
                     }
-                    if (!unique)
-                        break;
-                    num /= 10;
+                    if (unique)
+                    {
+                        if (f == 0)
+                            System.out.print(i);
+                        else
+                            System.out.print(", "+i);
+                        f++;
+                    }
                 }
-                if (unique)
-                {
-                    if (f == 0)
-                        System.out.print(i);
-                    else
-                        System.out.print(", "+i);
-                    f++;
-                }
+                System.out.println("\nFrequncy of unique-digit integers is: "+f);
             }
-            System.out.println("\nFrequncy of unique-digit integers is: "+f);
+            else
+                System.out.println("M and N must be less than 30000");
         }
         else
             System.out.println("M must be less than N");
